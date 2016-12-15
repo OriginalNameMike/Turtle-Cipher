a8d0a07769f6a8333f99e201182ba604665a1b23
OriginalNameMike <chipset-my@ukr.net>
2016-12-15 20:36:32 +0200

    Рисуем символы массонов.

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 from turtle import *
-border = 15
-letter_size = 30
+border = 20
+letter_size = 100
 letter_speceing = 20
 size = [800, 600]
 mideana =[0, 0] 
@@ -24,9 +24,53 @@ def pure_turtle(action):
 def pure_goto(point):
     pure_turtle(lambda: goto(point))
 
-pure_goto(mideana)
-goto(pos() + vector_right)
+def center_dot(size):
+    position = pos()
+    pure_goto(position + (letter_size / 2, letter_size / 2))
+    dot(size)
+    pure_goto(position)
 
+def down_line():
+    position = pos()
+    goto(pos() + (vector_right))
+    pure_goto(position)
+
+def upper_line():
+    position = pos()
+    pure_goto(pos() + vector_up)
+    goto(pos() + vector_right)
+    pure_goto(position) 
+    
+def left_line():
+    position = pos()
+    goto (pos() + vector_up)
+    pure_goto(position)
+
+def right_line():
+    position = pos()
+    pure_goto (pos() + vector_right)
+    goto (pos() + vector_up)
+    pure_goto(position)
+
+def letter_K():
+    position = pos()
+
+    goto(pos() + vector_up + (vector_right[0] / 2, vector_right[1] / 2)) 
+    goto(pos() + vector_down + (vector_right[0] / 2, vector_right[1] / 2))
+
+def letter_A():
+    down_line()
+    right_line()
+
+def letter_N():
+    down_line()
+    right_line()
+    center_dot(15)
+
+pure_goto(mideana)
+letter_A()
+letter_N()
+letter_K()
 input()
 
 
