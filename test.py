b5bb8d14d040fa783efd8f633bda25fa0fe69ad9
OriginalNameMike <chipset-my@ukr.net>
2016-12-16 19:00:14 +0200

    Все буквы еже на месте.

--- test.py
+++ test.py
@@ -16,6 +16,7 @@ vector_down = (0, -100)
 vector_left = (-100,0)
 vector_right = (100,0)
 
+
 def pure_turtle(action):
     up()
     action()
@@ -23,6 +24,8 @@ def pure_turtle(action):
 
 def pure_goto(point):
     pure_turtle(lambda: goto(point))
+
+pure_goto(mideana)
 
 def center_dot(size):
     position = pos()
@@ -54,23 +57,169 @@ def right_line():
 
 def letter_K():
     position = pos()
-
     goto(pos() + vector_up + (vector_right[0] / 2, vector_right[1] / 2)) 
     goto(pos() + vector_down + (vector_right[0] / 2, vector_right[1] / 2))
+    pure_goto(position)
+
+def letter_J():
+    position = pos()
+    pure_goto(pos() + vector_up)
+    goto(pos() + vector_down + (vector_right[0] / 2, vector_right[1] / 2)) 
+    goto(pos() + vector_up + (vector_right[0] / 2, vector_right[1] / 2))
+    pure_goto(position)
+
+def letter_W():
+    position = pos()
+    pure_goto(pos() + vector_up)
+    goto(pos() + vector_down + (vector_right[0] / 2, vector_right[1] / 2)) 
+    goto(pos() + vector_up + (vector_right[0] / 2, vector_right[1] / 2))
+    pure_goto(position)
+    center_dot(15)
+       
+def letter_X():
+    position = pos()
+    goto(pos() + vector_up + (vector_right[0] / 2, vector_right[1] / 2)) 
+    goto(pos() + vector_down + (vector_right[0] / 2, vector_right[1] / 2))
+    pure_goto(position)
+    center_dot(15)
+
+def letter_L():
+    position = pos()
+    goto(pos() + vector_right + (vector_up[0] / 2, vector_up[1] / 2 )) 
+    goto(pos() + vector_left + (vector_up[0] / 2, vector_up[1] / 2 ))
+    pure_goto(position)
+
+def letter_Y():
+    position = pos()
+    goto(pos() + vector_right + (vector_up[0] / 2, vector_up[1] / 2 )) 
+    goto(pos() + vector_left + (vector_up[0] / 2, vector_up[1] / 2 ))
+    pure_goto(position)
+    center_dot(15)
+
+def letter_M():
+    position = pos()
+    pure_goto(pos() + vector_right)
+    goto(pos() + vector_left + (vector_up[0] / 2, vector_up[1] / 2 )) 
+    goto(pos() + vector_right + (vector_up[0] / 2, vector_up[1] / 2 ))
+    pure_goto(position)
+
+def letter_Z():
+    position = pos()
+    pure_goto(pos() + vector_right)
+    goto(pos() + vector_left + (vector_up[0] / 2, vector_up[1] / 2 )) 
+    goto(pos() + vector_right + (vector_up[0] / 2, vector_up[1] / 2 ))
+    pure_goto(position)
+    center_dot(15)
 
 def letter_A():
     down_line()
     right_line()
+
+def letter_B():
+    left_line()
+    down_line()
+    right_line()
+
+def letter_O():
+    left_line()
+    down_line()
+    right_line()
+    center_dot(15)
+
+def letter_C():
+    left_line()
+    down_line()
+
+def letter_P():
+    left_line()
+    down_line()
+    center_dot(15)
+
+def letter_D():
+    down_line()
+    upper_line()
+    right_line()
+
+def letter_E():
+    down_line()
+    upper_line()
+    right_line()
+    left_line()
+
+def letter_R():
+    down_line()
+    upper_line()
+    right_line()
+    left_line()
+    center_dot(15)
+
+def letter_F():
+    down_line()
+    upper_line()
+    left_line()
+    
+def letter_S():
+    down_line()
+    upper_line()
+    left_line()
+    center_dot(15)
+
+def letter_Q():
+    down_line()
+    upper_line()
+    right_line()
+    center_dot(15)
 
 def letter_N():
     down_line()
     right_line()
     center_dot(15)
 
-pure_goto(mideana)
-letter_A()
-letter_N()
-letter_K()
+def letter_G():
+    upper_line()
+    right_line()
+    
+def letter_T():
+    upper_line()
+    right_line()
+    center_dot(15)
+
+def letter_H():
+    upper_line()
+    right_line()
+    left_line()
+
+def letter_U():
+    upper_line()
+    right_line()
+    left_line()
+    center_dot(15)
+
+def letter_I():
+    upper_line()
+    left_line()
+
+def letter_V():  
+    upper_line()
+    left_line()
+    center_dot(15)
+
+def exec_letter(letter):
+    current_funk = "letter_" + letter
+    if current_funk in globals():
+        globals()[current_funk]()
+
+
+def encript(text):
+    text = text.upper()
+    for letter in text:
+        exec_letter(letter)
+        position = pos()
+        pure_goto(position + vector_right + (letter_speceing, 0))
+
+encript("MISha")
+
+
 input()
 
 
